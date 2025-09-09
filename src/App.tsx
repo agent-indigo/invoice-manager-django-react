9cbc744b32d50d9f2adcf94f13869d8ea55f2631
agent-indigo <bradenhynes@gmail.com>
2025-09-08 22:43:08 -0230

    Bug fix and added superuser creation

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,20 @@ import {
 } from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import {ToastContainer} from 'react-toastify'
-import {Provider} from 'react-redux'
 import Footer from './components/Footer'
-import store from './redux/store'
 import 'bootswatch/dist/united/bootstrap.css'
 import 'react-toastify/dist/ReactToastify.css'
 const App: FunctionComponent = (): ReactElement => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <main className="py-3">
-        <Container>
-          <Routes>
-            
-          </Routes>
-        </Container>
-      </main>
-      <Footer/>
-      <ToastContainer/>
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <main className="py-3">
+      <Container>
+        <Routes>
+          
+        </Routes>
+      </Container>
+    </main>
+    <Footer/>
+    <ToastContainer/>
+  </BrowserRouter>
 )
 export default App
