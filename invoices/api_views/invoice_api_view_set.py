06a32d01833754aa74a1e059321abb2f206099e0
agent-indigo <bradenhynes@gmail.com>
2025-09-10 10:20:07 -0230

    Fixed invoice PATCH handling

--- invoices/api_views/invoice_api_view_set.py
+++ invoices/api_views/invoice_api_view_set.py
@@ -1,15 +1,18 @@
 """
 Invoice API view set
 """
+from urllib.request import Request
 from django.db.models import QuerySet
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from ..models import Invoice
 from ..serializers import InvoiceSerializer
 class InvoiceApiViewSet(ModelViewSet):
     """
     Invoice API view set
     """
+    # pylint: disable = no-member
     queryset = Invoice.objects.none()
     serializer_class = InvoiceSerializer
     permission_classes = [
@@ -19,6 +22,7 @@ class InvoiceApiViewSet(ModelViewSet):
         """
         Get a single invoice
         """
+        # pylint: disable = no-member
         return Invoice.objects.get(
             id = self.kwargs.get('pk'),
             user_id = self.request.user.id
@@ -27,6 +31,7 @@ class InvoiceApiViewSet(ModelViewSet):
         """
         Get all invoices owned by the currently logged in user
         """
+        # pylint: disable = no-member
         return Invoice.objects.filter(
             user_id = self.request.user.id
         )
@@ -42,12 +47,26 @@ class InvoiceApiViewSet(ModelViewSet):
         )
     def partial_update(
         self: 'InvoiceApiViewSet',
-        serializer: InvoiceSerializer
-    ) -> Invoice:
+        request: Request,
+        *args: tuple[str],
+        **kwargs: dict[
+            str,
+            str
+        ]
+    ) -> Response:
         """
         Update an invoice
         """
-        return serializer.save()
+        serializer = self.get_serializer(
+            self.get_object(),
+            data = request.data,
+            partial = True
+        )
+        serializer.is_valid(
+            raise_exception = True
+        )
+        self.perform_update(serializer)
+        return Response(serializer.data)
     def perform_destroy(
         self: 'InvoiceApiViewSet',
         instance: Invoice
