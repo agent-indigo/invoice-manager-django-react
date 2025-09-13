13b3d98db8f8b1da0c6a6da0606e740c8d828135
agent-indigo <bradenhynes@gmail.com>
2025-09-12 22:25:00 -0230

    Completed home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,13 +2,66 @@ import {
   FunctionComponent,
   ReactElement
 } from 'react'
+import {
+  NavigateFunction,
+  useNavigate
+} from 'react-router-dom'
+import {
+  Button,
+  Card
+} from 'react-bootstrap'
+import {
+  FaFileInvoiceDollar,
+  FaKey,
+  FaUserPlus
+} from 'react-icons/fa'
 import {Helmet} from 'react-helmet'
+import ContextProps from '@/types/ContextProps'
+import {useGetContext} from '../components/ContextProvider'
 const HomePage: FunctionComponent = (): ReactElement => {
+  const navigate: NavigateFunction = useNavigate()
+  const {user}: ContextProps = useGetContext()
   return (
     <>
       <Helmet>
-        <title>Home | Invoices</title>
+        <title>
+          Home | Invoices
+        </title>
       </Helmet>
+      <Card
+        bg='light'
+        className='p-auto'
+      >
+        {user ? (
+          <Button
+            type='button'
+            variant='primary'
+            className='p-auto text-white'
+            onClick={(): void => navigate('/invoices')}
+          >
+            <FaFileInvoiceDollar/> Invoices
+          </Button>
+        ) : (
+          <>
+            <Button
+              type='button'
+              variant='primary'
+              className='p-auto text-white'
+              onClick={(): void => navigate('/login')}
+            >
+              <FaKey/> Log In
+            </Button>
+            <Button
+              type='button'
+              variant='primary'
+              className='p-auto text-white'
+              onClick={(): void => navigate('/register')}
+            >
+              <FaUserPlus/> Register
+            </Button>
+          </>
+        )}
+      </Card>
     </>
   )
 }
